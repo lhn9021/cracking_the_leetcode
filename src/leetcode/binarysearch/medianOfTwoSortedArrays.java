44f52b95cc56ea5cf1798cfaec17466a75a1413f
neal <neal.lee21@gmail.com>
2014-02-13 15:02:11 -0500

    add comment in medianOfTwoSortedArrays

--- src/leetcode/binarysearch/medianOfTwoSortedArrays.java
+++ src/leetcode/binarysearch/medianOfTwoSortedArrays.java
@@ -7,6 +7,10 @@ package leetcode.binarysearch;
  * http://oj.leetcode.com/problems/median-of-two-sorted-arrays/
  * 
  * Hint: use find Kth element to find the median of two sorted arrays
+ * a is separated into a.start to No.pa and (No.pa+1) to a.end, b is separated into b.start to No. (k-pa), No. (k-pa+1) to b.end
+ * if a[a_start+pa-1]<b[b_start+pb-1] because there are k-pa elem in "b" and a.size-pa elem in "a" larger
+ * than all a.start to No.pa, which size is Math.min(a_size,k/2), the k th element cannot be in a.start to No.pa. 
+ * So we discard this part, update k to k-pa and a.start and a.size and continue searching. vice versa.
  * 
  */
 public class medianOfTwoSortedArrays {
