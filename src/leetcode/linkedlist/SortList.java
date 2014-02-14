d3f3a1e93d9051dc4821992349b74b4dc22194af
neal <neal.lee21@gmail.com>
2014-02-13 22:32:24 -0500

    modify variable name in sortlist

--- src/leetcode/linkedlist/SortList.java
+++ src/leetcode/linkedlist/SortList.java
@@ -30,29 +30,25 @@ public class SortList {
 		}
 
 		private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-			if (l1 == null)
-				return l2;
-			if (l2 == null)
-				return l1;
-			ListNode prev = new ListNode(0);
-			ListNode save = prev;
-			int a, b;
-			while (l1 != null && l2 != null) {
-				if (l1.val <= l2.val) {
-					prev.next = l1;
-					l1 = l1.next;
-				} else {
-					prev.next = l2;
-					l2 = l2.next;
-
-				}
-				prev = prev.next;
-			}
-			if (l1 != null)
-				prev.next = l1;
-			else if (l2 != null)
-				prev.next = l2;
-			return save.next;
-		}
+	        if(l1==null) return l2;
+	        if(l2==null) return l1;
+	        ListNode begin=new ListNode(0);
+	        ListNode prev=begin;
+	        int a,b;
+	        while(l1!=null&&l2!=null){
+	            if(l1.val<=l2.val){
+	                prev.next=l1;
+	                l1=l1.next;
+	            }else{
+	                prev.next=l2;
+	                l2=l2.next;
+	                
+	            }
+	            prev=prev.next;
+	        }
+	        if(l1!=null) prev.next=l1;
+	        else if(l2!=null) prev.next=l2;
+	        return begin.next;
+	    }
 	}
 }
