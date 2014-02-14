2f16cb5141286cf2f53d4e5195025d086bed634a
neal <neal.lee21@gmail.com>
2014-02-14 18:46:16 -0500

    add UniqueBinarySearchTrees

---
+++ src/leetcode/tree/UniqueBinarySearchTrees.java
@@ -0,0 +1,14 @@
+package leetcode.tree;
+/*
+ * Unique Binary Search Trees
+ * 
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ * 
+ * http://oj.leetcode.com/problems/unique-binary-search-trees/
+ * 
+ * Hint: Catalan number
+ * http://en.wikipedia.org/wiki/Catalan_number
+ */
+public class UniqueBinarySearchTrees {
+
+}
