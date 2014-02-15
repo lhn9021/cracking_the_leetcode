3eda6042953286e98abb38b6c9ba33788810fccb
neal <neal.lee21@gmail.com>
2014-02-14 19:12:48 -0500

    add ConvertSortedArrayToBinarySearchTree

---
+++ src/leetcode/tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,27 @@
+package leetcode.tree;
+/*
+ * Convert Sorted Array to Binary Search Tree 
+ * 
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ * 
+ * http://oj.leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+ * 
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+	public class Solution {
+	    public TreeNode sortedArrayToBST(int[] num) {
+	        // Note: The Solution object is instantiated only once and is reused by each test case.
+	        if(num.length==0) return null;
+	        return bst(num,0,num.length-1);
+	        
+	    }
+	    public TreeNode bst(int[]num,int start,int end){
+	        if(start>end) return null;
+	        int median=(start+end)/2;
+	        TreeNode root=new TreeNode(num[median]);
+	        root.left=bst(num,start,median-1);
+	        root.right=bst(num,median+1,end);
+	        return root;
+	    }
+	}
+}
