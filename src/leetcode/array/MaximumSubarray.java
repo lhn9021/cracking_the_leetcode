2022789f2d73ecb2c77cd275df2ccf878211ed9b
neal <neal.lee21@gmail.com>
2014-02-23 13:57:19 -0500

    Simplify maximum subarray

--- src/leetcode/array/MaximumSubarray.java
+++ src/leetcode/array/MaximumSubarray.java
@@ -12,19 +12,29 @@ package leetcode.array;
  * Also we need to compare subarrray that starts at i
  */
 public class MaximumSubarray {
-	public class Solution {
-		public int maxSubArray(int[] A) {
-		    int maxnow = 0;
-		    int maxall = A[0];
-		    for(int i=0;i<A.length;i++){
-		        maxnow=Math.max(0,maxnow+A[i]);
-		        if(maxnow>0){
-		            maxall=Math.max(maxall,maxnow);
-		        }else{
-		            maxall=Math.max(maxall,A[i]);
-		        }
-		    }
-		    return maxall;
-		}
-	}
+//	public class Solution {
+//		public int maxSubArray(int[] A) {
+//		    int maxnow = 0;
+//		    int maxall = A[0];
+//		    for(int i=0;i<A.length;i++){
+//		        maxnow=Math.max(0,maxnow+A[i]);
+//		        if(maxnow>0){
+//		            maxall=Math.max(maxall,maxnow);
+//		        }else{
+//		            maxall=Math.max(maxall,A[i]);
+//		        }
+//		    }
+//		    return maxall;
+//		}
+//	}
+	//when currentSum is negative, currentSum+A[i]<A[i], so we start a new beginning same as greedy
+	//when currentSum is positive, currentSum+A[i]>A[i] so we add A[i]
+	 public int maxSubArray(int[] A) {
+	        int currentSum=A[0],maxSum=A[0];
+	        for(int i=1;i<A.length;i++){
+	            currentSum=Math.max(currentSum+A[i],A[i]);
+	            maxSum=Math.max(maxSum,currentSum);
+	        }
+	        return maxSum;
+	    }
 }
